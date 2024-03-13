9142ad94a4ba42b4b4b8ba5a097bebd60b370088
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-03-13 21:06:31 +0530

    Added serializers.py

--- EmpComp/EmpComp/serializers.py
+++ EmpComp/EmpComp/serializers.py
@@ -5,3 +5,8 @@ class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
         fields = '__all__'
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = '__all__'
